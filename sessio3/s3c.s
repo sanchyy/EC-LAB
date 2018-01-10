e3c828479bc8952ada66ead4b66df33643c23e10
Sanchyy <sanchez.gassull@gmail.com>
2018-01-10 10:19:49 +0100

    sessio3

--- sessio3/s3c.s
+++ sessio3/s3c.s
@@ -1,38 +1,45 @@
-        # Sessio 3
+	# Sessio 3
 
-        .data
-mat:    .word 0,0,2,0,0,0
-        .word 0,0,4,0,0,0
-        .word 0,0,6,0,0,0
-        .word 0,0,8,0,0,0
-        .align 2
-resultat: .space 4
-        .text
-        .globl main
+	.data
+mat:	.word 0,0,2,0,0,0
+		.word 0,0,4,0,0,0
+		.word 0,0,6,0,0,0
+		.word 0,0,8,0,0,0
+
+result:	.space 4
+
+	.text
+	.globl main
+
 main:
-# Escriu aqui el teu codi del main
-addiu $sp,$sp,-4
-sw $ra,0($sp)
-la $a0,mat
-jal suma_col
 
-la $t0,resultat
-sw $v0,0($t0)
-lw $ra,0($sp)
-addi $sp,$sp,4
+	addiu $sp,$sp,-4	#obrim pila
+	sw $ra,0($sp)		#no reg.segurs -> guardem $ra nomes
+
+	la $a0, mat			#guardem @ a mat
+	jal suma_col		#anem a la rutina
+
+	lw $ra,0($sp)		#carreguem $ra un altre cop
+	addiu $sp,$sp,4		#tanquem pila
+
+	la $t7, result		#adreça a resultat
+	sw $v0, 0($t7)		#guardem el resultat de la funcio a resultat
+
+	jr $ra
+
 
 suma_col:
-# Escriu aqui el teu codi de la subrutina
-move $t0, $zero
-move $t1, $zero
-addiu $t2, $a0, 8
-li $t3, 4
+	
+	move $t0, $zero		#i = 0
+ 	li $t3, 4 			#comparacio amb 4
+	move $v0, $zero 	#suma = 0	
+	addiu $t1,$a0,8 	#punter a m[0][2]
 
 for:
-lw $t4, 0($t2)
-addu $t1, $t1, $t4
-addiu $t2, $t2, 24
-addiu $t0, $t0, 1
-blt $t0, $t3, for
-move $v0, $t1
-jr $ra
+	lw $t2,0($t1) 		#m[i][2]
+	addu $v0,$v0,$t2	#suma += m[i][2]
+	addiu $t1,$t1,24 	#sumar stride al punter (6*4)
+	addiu $t0,$t0,1		#++i
+	blt $t0,$t3,for		#condicio i < 4
+
+	jr $ra
